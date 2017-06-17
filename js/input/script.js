2cd0be0ea756f24e90645769f1f028cc9ff2cbac
Xenos <t103590019@ntut.edu.tw>
2017-06-18 03:20:13 +0800

    update scroll effect

--- js/input/script.js
+++ js/input/script.js
@@ -5,4 +5,18 @@ $(document).ready(function() {
 		$(".navbar__icon").toggleClass("active");
 		$(".navbar__content").children().slideToggle(200);
 	});
+
+	$(".navbar__content li").click(function() {
+		var id = $(this).children().attr('href');
+		var topValue = $(id).offset().top;
+		var navbarHeight = 60;
+
+		$(".navbar__icon").toggleClass("active");
+		$(".navbar__content").children().slideToggle(200);
+		$("html body").stop().animate({
+			scrollTop: topValue - navbarHeight
+		}, 1000);
+
+		//console.log($(id).offset().top);
+	});
 });
